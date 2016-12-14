dd09b62c8a66fd12709a62480f0ca907f5b1967b
Gianluca Rossi <gianluca.rossi@farfetch.com>
2016-12-14 15:53:31 +0000

    BUG: ensure user can't change nickname to one already existing

--- app/views.py
+++ app/views.py
@@ -111,13 +111,13 @@ def user(nickname):
 @app.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit():
-    form = EditForm()
+    form = EditForm(g.user.nickname)
     if form.validate_on_submit():
         g.user.nickname = form.nickname.data
         g.user.about_me = form.about_me.data
         db.session.add(g.user)
         db.session.commit()
-        flash('Your changes ahve been saved.')
+        flash('Your changes have been saved.')
         return redirect(url_for('edit'))
     else:
         form.nickname.data = g.user.nickname
