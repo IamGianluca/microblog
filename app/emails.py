a83910bd603341b94137eebf4e6875a783e5e98f
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2016-12-20 11:44:57 +0100

    ENH: send async email

--- app/emails.py
+++ app/emails.py
@@ -1,15 +1,23 @@
+from threading import Thread
+
 from flask import render_template
 from flask_mail import Message
 
-from app import mail
+from app import app, mail
+from app.decorators import async
 from config import ADMINS
 
+
+@async
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    send_async_email(app, msg)
 
 def follower_notification(followed, follower):
     send_email('[microblog] {} is now following '
